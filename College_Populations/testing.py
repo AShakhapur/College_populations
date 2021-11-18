31e75d2b2c97bddd20e5fba1f44e560334e4defb
AShakhapur <91990936+AShakhapur@users.noreply.github.com>
2021-11-18 18:29:00 -0500

    Add files via upload

---
+++ College_Populations/testing.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import sqlite3
+
+# Project Goals: Create a visualization of the gender gap in colleges in the US
+# Analyze the data based on geographic location
+# Perform statistical analysis to determine whether or not there is a significant difference between the two genders
+# Setup a sql database to store the data
+# Create an interface that allows users to get the results they want, whether that be overall or of a specific college or region
+ 
+
+# Work Plan
+# Pull the data either using a single api, webscraping, or manually take the data from a variety of sources
+    # Manually would make the program less useful to others and in the future
+
+# Store the data in an sqlite database
+
+# Clean and process the data
+    # Possibly normalizing and modelling the data
+
+# Analyze the difference based on the relationship between gender and geographic location
+    # Create a variety of visualizations and build a written conclusion with a timestamp
+
+# Create an interface that allows users to get the specific data and conclusion they want
+ 
